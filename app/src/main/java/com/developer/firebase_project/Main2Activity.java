c41d2b4067718dd2daded0725a70e9da9e2ef99d
Shreeya Bhuwalka <shreeyabhuwalka@gmail.com>
2020-05-21 00:48:04 +0530

    Uploading images to Firebase Storage. (Don't forget to add permissions)

--- app/src/main/java/com/developer/firebase_project/Main2Activity.java
+++ app/src/main/java/com/developer/firebase_project/Main2Activity.java
@@ -4,16 +4,25 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -29,6 +38,9 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.google.firebase.firestore.SetOptions;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -38,7 +50,9 @@ import java.util.Map;
 public class Main2Activity extends AppCompatActivity {
      Button add;
     private EditText input;
-
+    private Uri imageUri;
+    private static final int IMAGE_REQUEST =2;
+    /////---------*IMPORTANT*-----Add permissions in Manifest file for reading external files----------//////
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,162 +67,73 @@ public class Main2Activity extends AppCompatActivity {
                 FirebaseAuth.getInstance().signOut();
             }
         });
-//        FirebaseDatabase.getInstance().getReference().child("ProgrammingKnowledge")
-//                .child("Name").setValue("Shreeya");
-//        HashMap<String,Object> map = new HashMap<>();
-//        map.put("Name","Shreeya");
-//        map.put("Email","shreeyabhuwalka@gmail.com");
-//
-//        FirebaseDatabase.getInstance().getReference()
-//                .child("ProgrammingKnowledge")
-//                .child("Multiple values")
-//                .updateChildren(map);
-        final int[] count = {0};
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String text = input.getText().toString();
-                if(text.isEmpty())
-                {
-                    Toast.makeText(Main2Activity.this, "Enter Name", Toast.LENGTH_SHORT).show();
-
-                }
-                else
-                {
-                    count[0]++;
-                    FirebaseDatabase.getInstance().getReference().child("language").child(text+ count[0]).setValue(text);
-                }
+                openImage();
             }
         });
         final ArrayList<String> list = new ArrayList<>();
         final ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.list_item,list);
         listView.setAdapter(adapter);
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("information");
-        reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                list.clear();
-                for(DataSnapshot snapshot:dataSnapshot.getChildren())
-                {
-                    information info = snapshot.getValue(information.class);
-                    String txt = null;
-                    if (info != null) {
-                        txt = info.getName() + " : " + info.getEmail();
-                    }
-                    else
-                        txt = "empty";
-//                     list.add(snapshot.getValue().toString());
-                    list.add(txt);
+    }
+
+    private void openImage() {
+        Intent intent = new Intent();
+        intent.setType("image/");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,IMAGE_REQUEST);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode==IMAGE_REQUEST)
+        {
+
+            imageUri = data.getData();
+            Log.i("Download", "Begins");
+            uploadImage();
+        }
+    }
+
+
+    private void uploadImage() {
+
+        final ProgressDialog pd = new ProgressDialog(this);
+        pd.setMessage("Uploading");
+        pd.show();
+
+        if (imageUri != null) {
+           final StorageReference fileRef = FirebaseStorage.getInstance().getReference().child("uploads").child(System.currentTimeMillis() + "." + getFileExtension(imageUri));
+            Log.i("Download", "ImageUri not null");
+
+            fileRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                    Log.i("Download", "ImageUri not null");
+                    fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String url = uri.toString();
+
+                            Log.i("Download", url);
+                            pd.dismiss();
+                            Toast.makeText(Main2Activity.this, "Image upload successful1", Toast.LENGTH_SHORT).show();
+                        }
+                    });
                 }
-                adapter.notifyDataSetChanged();
-            }
+            });
+        }
 
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
+    }
 
-            }
-        });
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
-        //--------------??????below command is to add data in cloud firestore-------------
-
-//        Map<String,Object> city =new HashMap<>();
-//        city.put("name","Varanasi");
-//        city.put("State","UP");
-//        city.put("Country","India");
-//        db.collection("Cities").document("JRS").set(city).addOnCompleteListener(new OnCompleteListener<Void>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Void> task) {
-//           if(task.isSuccessful())
-//           {
-//               Toast.makeText(Main2Activity.this, "Value Added", Toast.LENGTH_SHORT).show();
-//           }
-//        }
-//        });
-
-
-        /////----------??????below command is to merge data to firestore data----------
-
-//        Map<String,Object> data = new HashMap<>();
-//        data.put("capital",false);
-//        db.collection("Cities").document("JRS").set(data, SetOptions.merge()).addOnCompleteListener(new OnCompleteListener<Void>() {
-//            @Override
-//            public void onComplete(@NonNull Task<Void> task) {
-//           if(task.isSuccessful())
-//           {
-//               Toast.makeText(Main2Activity.this, "Merge Successful", Toast.LENGTH_SHORT).show();
-//           }
-//        }
-//        });
-
-
-
-        /////----------??????below command is to add data with ***unique id*** in firestore data----------
-//        Map<String,Object> data = new HashMap<>();
-//        data.put("Name","Tokyo");
-//        data.put("Capital","Japan");
-//        db.collection("Cities").add(data).addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentReference> task) {
-//                if(task.isSuccessful())
-//           {
-//               Toast.makeText(Main2Activity.this, "Merge Successful", Toast.LENGTH_SHORT).show();
-//           }
-//            }
-//        });
-
-
-        /////----------??????below command is to update existing data in firestore data----------
-//        DocumentReference ref = FirebaseFirestore.getInstance().collection("Cities").document("JRS");
-//        ref.update("capital",true);
-
-
-        /////----------??????below command is to retrieve data from firestore data----------
-
-//        DocumentReference docRef = FirebaseFirestore.getInstance().collection("Cities").document("JRS");
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                    if(task.isSuccessful())
-//                    {
-//                        DocumentSnapshot doc = task.getResult();
-//                        if(doc.exists())
-//                        {
-//                            Log.i("Document", doc.getData().toString());
-//                        }
-//                        else
-//                        {
-//                            Log.i("Document","No Data");
-//                        }
-//                    }
-//                }
-//
-//        });
-
-
-        /////----------??????below command is to retrieve data from firestore data----------
-//        FirebaseFirestore.getInstance().collection("Cities").whereEqualTo("capital",true)
-//                .get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<QuerySnapshot> task) {
-//                if(task.isSuccessful())
-//                {
-//                    for(QueryDocumentSnapshot doc:task.getResult())
-//                    {
-//                        Log.i("Document",doc.getId()+ " : " + doc.getData());
-//                    }
-//                }
-//            }
-//        });
-
-
-        /////----------??????below command is to retrieve realtime data from particular document of firestore data----------
-        FirebaseFirestore.getInstance().collection("Cities").document("JRS")
-                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
-                    @Override
-                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                        ////type code here
-                    }
-                });
+    private String getFileExtension(Uri uri) {
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
     }
 }
